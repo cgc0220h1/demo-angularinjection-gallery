3d7778a368d6da1cdc464155e070a735d2e1a1ae
Duc <vanduc2514@gmail.com>
2020-07-08 16:03:24 +0700

    Show Image on Image Card

--- src/app/image-gallery/image-card/image-card.component.ts
+++ src/app/image-gallery/image-card/image-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-image-card',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageCardComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  src = '';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
--- src/app/image-gallery/image-gallery.component.ts
+++ src/app/image-gallery/image-gallery.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image-gallery.component.scss']
 })
 export class ImageGalleryComponent implements OnInit {
+  listImage = [
+    'https://via.placeholder.com/1280x420/e91e63/ffffff?text=1',
+    'https://via.placeholder.com/1280x420/e91e63/ffffff?text=2',
+    'https://via.placeholder.com/1280x420/e91e63/ffffff?text=3',
+    'https://via.placeholder.com/1280x420/e91e63/ffffff?text=4'
+  ];
 
   constructor() { }
 
