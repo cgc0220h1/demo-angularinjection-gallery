64fd0709918132073eb3fb338e6d1823327668f4
Duc <vanduc2514@gmail.com>
2020-07-08 17:37:29 +0700

    Add and show Image Slider component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {ImageGalleryModule} from './image-gallery/image-gallery.module';
 import {GalleryConfig} from './image-gallery/token';
+import {ImgSliderModule} from './img-slider/img-slider.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import {GalleryConfig} from './image-gallery/token';
   ],
   imports: [
     BrowserModule,
-    ImageGalleryModule
+    ImageGalleryModule,
+    ImgSliderModule
   ],
   providers: [
     {provide: GalleryConfig, useValue: 2}
