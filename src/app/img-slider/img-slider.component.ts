8a09112cbaaa2f8cf5bfb87e2b5d01eacdf216a8
Duc <vanduc2514@gmail.com>
2020-07-09 09:11:28 +0700

    Change showing method of img slide

--- src/app/img-slider/img-slider.component.ts
+++ src/app/img-slider/img-slider.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-img-slider',
   templateUrl: './img-slider.component.html',
-  styleUrls: ['./img-slider.component.scss']
+  styleUrls: ['./img-slider.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ImgSliderComponent implements OnInit {
 
